a1164f2a00d65fcfcc876527ae52619b85fc3fc5
Melania Morales <melaniacrc@gmail.com>
2020-02-18 13:14:53 -0600

    Graficar Puntos

---
+++ TAREA1/Assets/Dibujar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dibujar : MonoBehaviour
+{
+    public int x;
+    public GameObject punto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(punto, new Vector3(5, 5, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
